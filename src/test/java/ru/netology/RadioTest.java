aa2b82c0565058b947f6ba60bce6c9a100232371
ansty <anastasiarodionova086@gmail.com>
2025-11-29 02:11:42 +0300

    Implemente flexible radio with configurable stations and tests

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -41,6 +41,7 @@ public class RadioTest {
         radio.next();
         assertEquals(0, radio.getCurrentStation());
     }
+
     @Test
     void shouldSwitchToPrevStation() {
         Radio radio = new Radio();
@@ -110,5 +111,59 @@ public class RadioTest {
             radio.increaseVolume();
         }
         assertEquals(50, radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldCreateRadioCustomStations() {
+        Radio radio = new Radio(15);
+        assertEquals(15, radio.getStationCount());
+    }
+
+    @Test
+    void shouldCreateRadioWithDefaultStations() {
+        Radio radio = new Radio();
+        assertEquals(10, radio.getStationCount());
+    }
+
+    @Test
+    void shouldHandleZeroStations() {
+        Radio radio = new Radio(0);
+        assertEquals(10, radio.getStationCount());
+    }
+
+    @Test
+    void shouldHandleNegativeStations() {
+        Radio radio = new Radio(-5);
+        assertEquals(10, radio.getStationCount());
+    }
+
+    @Test
+    void shouldSetStationWithCustomRange() {
+        Radio radio = new Radio(15);
+        radio.setCurrentStation(14);
+        assertEquals(14, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldNotSetStationAboveCustomMax() {
+        Radio radio = new Radio(15);
+        radio.setCurrentStation(15);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldSwitchFromLastToFirstCustom() {
+        Radio radio = new Radio(15);
+        radio.setCurrentStation(14);
+        radio.next();
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldSwitchFromFirstToLastCustom() {
+        Radio radio = new Radio(15);
+        radio.setCurrentStation(0);
+        radio.prev();
+        assertEquals(14, radio.getCurrentStation());
     }
 }
